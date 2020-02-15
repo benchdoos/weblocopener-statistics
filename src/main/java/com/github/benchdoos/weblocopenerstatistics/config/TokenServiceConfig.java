d38b936fcf68b35887762637cff39f7d53145e0b
Eugene Zrazhevsky <eugeny.zrazhevsky@gmail.com>
2020-02-15 20:52:54 +0300

    Removed redis. Will be using mongodb. (free storage)

--- src/main/java/com/github/benchdoos/weblocopenerstatistics/config/TokenServiceConfig.java
+++ src/main/java/com/github/benchdoos/weblocopenerstatistics/config/TokenServiceConfig.java
@@ -3,17 +3,15 @@ package com.github.benchdoos.weblocopenerstatistics.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 
 @RequiredArgsConstructor
 @Configuration
 public class TokenServiceConfig {
-    private final RedisConnectionFactory redisConnectionFactory;
 
     @Bean
     public TokenStore tokenStore() {
-        return new RedisTokenStore(redisConnectionFactory);
+        return new InMemoryTokenStore();
     }
 }
